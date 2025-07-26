7eed5f4a726a1381d6ab89ca470c4e32aeaf745a
Maxim Menshikov <maxim@menshikov.org>
2025-07-26 18:23:21 +0100

    Test rootfs build

---
+++ 00_test_build_rootfs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+export TOP_DIR="$(cd "$(dirname "$(which "$0")")" ; pwd -P)"
+
+tmp_dir="${TOP_DIR}/tmp/test-build-rootfs"
+
+apt-get update -y
+apt-get install -y xz-utils git debootstrap
+
+cd "${TOP_DIR}"
+
+mkdir -p "${tmp_dir}"
+
+pushd "${tmp_dir}"
+    git clone https://github.com/dotnet/runtime
+    pushd runtime
+        ./eng/common/cross/build-rootfs.sh riscv64 noble --skipunmount --rootfsdir $(pwd)/.tools/rootfs/riscv64
+    popd
+popd
+exit 1
