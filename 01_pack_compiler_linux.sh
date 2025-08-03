b66062015d2e323a0022cbd7321e406497b3c27d
Maxim Menshikov <maxim@menshikov.org>
2025-08-03 09:14:11 +0100

    compiler-linux: put everything to bin folder

--- 01_pack_compiler_linux.sh
+++ 01_pack_compiler_linux.sh
@@ -8,25 +8,25 @@ apt-get update -y
 apt-get install -y llvm-18 lld-18 patchelf binutils zip
 
 output="${TOP_DIR}/output/compiler-linux"
-mkdir -p "${output}"
+mkdir -p "${output}/bin"
 
 pushd /usr/lib/x86_64-linux-gnu
-cp libffi.so* "$output/"
-cp libedit.so.2* "$output/"
-cp libxml2.so* "$output/"
+cp libffi.so* "$output/bin"
+cp libedit.so.2* "$output/bin"
+cp libxml2.so* "$output/bin"
 popd
 
 pushd /usr/lib/llvm-18/lib
-cp libLLVM.so* "$output/"
+cp libLLVM.so* "$output/bin"
 popd
 
 pushd /usr/lib/llvm-18/bin
-cp lld "$output/"
-cp llvm-objcopy "$output/"
+cp lld "$output/bin"
+cp llvm-objcopy "$output/bin"
 popd
 
 pushd "$output"
-	patchelf --set-rpath '$ORIGIN' *
+	patchelf --set-rpath '$ORIGIN' bin/*
 	if [ -f "$output_file" ] ; then
 		rm "$output_file"
 	fi
