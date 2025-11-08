e76aef465c4fc777fedf8e47117bffc4b60e5a7a
Maxim Menshikov <maxim@menshikov.org>
2025-11-08 18:15:44 +0000

    Ignore failures when applying Alpine patch

--- patch_runtime.sh
+++ patch_runtime.sh
@@ -6,7 +6,8 @@ pushd dotnet/src/runtime
     for file in $(ls ${TOP_DIR}/patches/bflat-runtime/*.patch | xargs) ; do
         echo Applying $file
         patch -p1 < $file
-        if [ "$?" != "0" ] ; then
+        res="$?"
+        if [ "$file" != "${TOP_DIR}/patches/bflat-runtime/12_alpine_custom.patch"] && [ "$res" != "0" ] ; then
             echo Failed to apply patch $file >&2
             exit 1
         fi
